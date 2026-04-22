836910f63cf83f853c9978bc63751d4feb7b5c62
krxxg04 <u202312629@upc.edu.pe>
2026-04-22 14:29:10 -0500

    feat: add allowedCommonJsDependencies to Angular configuration.

--- sc-osito/src/app/app.ts
+++ sc-osito/src/app/app.ts
@@ -1,13 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalculadoraComponent],
+  imports: [CalculadoraComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('sc-osito');
 }
